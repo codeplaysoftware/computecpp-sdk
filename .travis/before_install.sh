a42d9c203d2929590fec834edc5db02559290ab2
Duncan McBain <duncan@codeplay.com>
2017-05-25 12:35:56 +0100

    Change to 20 OCL

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -15,10 +15,10 @@ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /u
 wget https://github.com/KhronosGroup/OpenCL-Headers/archive/master.zip -O /tmp/ocl-headers.zip
 unzip /tmp/ocl-headers.zip -d /tmp
 git clone https://github.com/KhronosGroup/OpenCL-ICD-Loader.git /tmp/OpenCL-ICD-Loader
-ln -sf /tmp/OpenCL-Headers-master/opencl21/CL /tmp/OpenCL-ICD-Loader/inc/CL
+ln -sf /tmp/OpenCL-Headers-master/opencl20/CL /tmp/OpenCL-ICD-Loader/inc/CL
 pushd /tmp/OpenCL-ICD-Loader && git checkout cb4acb99df3441e147bd859c20d9c3533ec95ce8 && make && popd
 # Recreate a fake OpenCL setup
 sudo mkdir /usr/include/CL/
-sudo cp -rf /tmp/OpenCL-Headers-master/opencl21/CL /usr/include/CL/
+sudo cp -rf /tmp/OpenCL-Headers-master/opencl20/CL /usr/include/CL/
 sudo cp -Rf /tmp/OpenCL-ICD-Loader/build/bin/* /usr/lib/
 sudo cp -Rf /tmp/OpenCL-ICD-Loader/build/bin/* /usr/lib/x86_64-linux-gnu/
