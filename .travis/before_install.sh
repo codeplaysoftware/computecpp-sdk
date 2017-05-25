3ba5662d8efbcbd574138a612bb4f42ff93c5f94
Duncan McBain <duncan@codeplay.com>
2017-05-25 17:58:54 +0100

    Remove TriSYCL while fixing ComputeCpp build

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -4,10 +4,7 @@ set -ev
 
 # Add modern dependencies (cmake, boost)
 sudo add-apt-repository ppa:george-edison55/cmake-3.x -y
-sudo add-apt-repository ppa:kzemek/boost -y
 sudo apt-get update -q
 sudo apt-get install cmake ocl-icd-libopencl1 ocl-icd-dev opencl-headers -y 
-# TriSYCL requires modern boost
-sudo apt-get install boost1.58 -y
 # Use gcc 5 as default
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
