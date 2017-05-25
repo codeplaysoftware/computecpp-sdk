b8972993571ecb05b28271d303d5ba693c0b3b3d
Duncan McBain <duncan@codeplay.com>
2017-05-25 11:42:44 +0100

    Update failing Travis config
    The Travis build now uses the OpenCL headers and ICD loader from the
    Ubuntu repository, as this is more reliable than the raw Git variants.
    Since we don't require an actual OpenCL device, this is still fine.
    
    Additionally, TriSYCL never worked properly for the SDK anyway, since
    we don't have proper CMake support for it. Therefore, it's been removed.
    This allows us to remove some dependencies from the Travis build script.
    Lastly, the greaaterequal problem was fixed in the last ComputeCpp
    release.

--- .travis/build_triSYCL.sh
+++
@@ -1,6 +0,0 @@
-##########################
-# Get triSYCL
-###########################
-
-wget https://github.com/triSYCL/triSYCL/archive/master.zip -O /tmp/trisycl.zip
-unzip /tmp/trisycl.zip -d /tmp  &> /dev/null 
