19fe39b329435e896b429d4e521cb25d72537307
Ruyman <ruyman@codeplay.com>
2017-07-06 15:42:58 +0100

    Initial commit of the Virtual Pointer (#5)
    * Initial commit of the Virtual Pointer
    
    This initial commit implements the basic structure of the virtual
    pointer interface for buffers, that enables allocation of deallocation
    of pointer-like structures that are not dereferenciable on the host
    but can be used to retrieve buffers and offsets later on.
    PointerMapper is a normal object, unlike the singleton implemented for
    the legacy pointer interface.
    See README.adoc for details.
    
    * Minor grammar fixes
    
    * Added fixes on readme after review
    
    * Template the PointerMapper class to allow different allocators
    
    * Clang-format files
    
    * Added more detailed explanation of the vptr
    
    * Task/11355 & task/11592: Add PointerMapper to the scheduler & remove template parameter from the PointerMapper (#6)
    
    * Added a test file for the functionality in the scheduler
    
    * Removed the template parameter from PointerMapper
    
    * Added a template parameter to get_buffer, as well as a static cast.
    
    * Removed template parameter for `get_buffer()`
    
    * Added a comment explaining buffer casting on get_buffer
    
    * Added clarifications to descriptions
    
    * Disabled the scheduler tests by default
    
    * Disabled testing of scheduler-specific features
    * Added initial documentation to the README
    
    * Pointer alias simplification
    
    The pointer alias headers don't need the reinterpret cast.
    
    * Editorial fixes
    
    * Editorial fixes
    
    * Cleaning pointer alias conversions
    
    * Task #11628: Add get_access() methods (#7)
    
    * Added get_accessor() methods.
    
    * Renamed get_accessor() to get_access()
    
    * Added tests for get_access()
    
    * Updated comments in the test files
    
    * Added Doxygen comments for get_access()
    
    * get_access() now returns a common_accessor...
    
      constructed with a buffer_mem
    
    * Restoring get_access() to use get_buffer()
    
    * Task #11630: Space-efficient memory management on the PointerMapper (#8)
    
    * Added some templates to experiment with memory
    
    * Added tests for benchmarking
    
    * New test benchmark problem properly
    
    * Added partitioning of space when reusing pointers
    
    * Fixed a bug: removing multiple elements of the
    same size was not adding them to the free list
    
    * Add partitioning of free address space
    when pointers are added
    
    * Fixed a bug: in remove_node() free end nodes not
    removed from m_free list
    
    * Fragmenting on add_pointer() now passes tests
    
    * In remove_pointer() fuse backwards
    
    * In remove_pointer() fuse forward
    
    * remove_pointer(): simplified the case of
    free pointers in the end of the map
    
    * Split fuse forward and backward into own functions
    
    * Small comment edits
    
    * Don't call get_node() in add_pointer()
    
    * Added implementation doc
    
    * Improvements in vptr document
    
    * Tests now use `get_[device|host]_ptr_as<>` methods

---
+++ utils/vptr/include/pointer_alias.hpp
@@ -0,0 +1,59 @@
+/***************************************************************************
+ *
+ *  Copyright (C) 2017 Codeplay Software Limited
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  For your convenience, a copy of the License has been included in this
+ *  repository.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Codeplay's ComputeCpp SDK
+ *
+ *  pointer_alias.hpp
+ *
+ *  Description:
+ *    Alias functions that obtain a pointer of the given type from an
+ *    accessor.
+ *
+ * Authors:
+ *
+ *    Ruyman Reyes   Codeplay Software Ltd.
+ *    Mehdi Goli     Codeplay Software Ltd.
+ *
+ **************************************************************************/
+
+#include <CL/sycl.hpp>
+
+#ifndef CL_SYCL_POINTER_ALIAS
+#define CL_SYCL_POINTER_ALIAS
+
+namespace cl {
+namespace sycl {
+namespace codeplay {
+
+template<typename T, typename AccessorT>
+typename cl::sycl::global_ptr<T>::pointer_t get_device_ptr_as(AccessorT& acc) {
+     return reinterpret_cast<typename cl::sycl::global_ptr<T>::pointer_t>
+              (&(acc.get_pointer()[0]));
+}
+
+template<typename T, typename AccessorT>
+T * get_host_ptr_as(AccessorT& acc) {
+     return reinterpret_cast<T *>(&(acc.get_pointer()[0]));
+}
+
+}  // codeplay
+}  // sycl
+}  // cl
+
+#endif  // CL_SYCL_POINTER_ALIAS
+
---
+++ utils/vptr/tests/offset.cc
@@ -0,0 +1,158 @@
+/***************************************************************************
+ *
+ *  Copyright (C) 2016 Codeplay Software Limited
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  For your convenience, a copy of the License has been included in this
+ *  repository.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Codeplay's ComputeCpp SDK
+ *
+ *  offset.cc
+ *
+ *  Description:
+ *   Test using offsets with the  pointer utility class
+ *
+ **************************************************************************/
+
+#include "gtest/gtest.h"
+
+#include <CL/sycl.hpp>
+#include <iostream>
+
+#include "pointer_alias.hpp"
+#include "virtual_ptr.hpp"
+
+using sycl_acc_target = cl::sycl::access::target;
+const sycl_acc_target sycl_acc_host = sycl_acc_target::host_buffer;
+const sycl_acc_target sycl_acc_buffer = sycl_acc_target::global_buffer;
+
+using sycl_acc_mode = cl::sycl::access::mode;
+const sycl_acc_mode sycl_acc_rw = sycl_acc_mode::read_write;
+
+using namespace codeplay;
+
+using buffer_t = PointerMapper::buffer_t;
+
+struct kernel {
+  using acc_type = cl::sycl::accessor<codeplay::buffer_data_type, 1,
+                                      sycl_acc_rw, sycl_acc_buffer>;
+  acc_type accB_;
+  int i_;
+  int j_;
+  int SIZE_;
+  int offset_;
+
+  kernel(acc_type accB, int i, int j, int SIZE, int offset)
+      : accB_(accB), i_(i), j_(j), SIZE_(SIZE), offset_(offset){};
+
+  void operator()() {
+    auto float_off = offset_ / sizeof(float);
+    float *ptr = cl::sycl::codeplay::get_device_ptr_as<float>(accB_);
+    ptr[float_off] = i_ * SIZE_ + j_;
+  };
+};
+
+TEST(offset, basic_test) {
+  PointerMapper pMap;
+  {
+    ASSERT_EQ(pMap.count(), 0u);
+    float *myPtr = static_cast<float *>(SYCLmalloc(100 * sizeof(float), pMap));
+
+    ASSERT_NE(myPtr, nullptr);
+
+    ASSERT_FALSE(PointerMapper::is_nullptr(myPtr));
+
+    ASSERT_EQ(pMap.count(), 1u);
+
+    myPtr += 3;
+
+    auto b = pMap.get_buffer(myPtr);
+
+    size_t offset = pMap.get_offset(myPtr);
+    ASSERT_EQ(offset, 3 * sizeof(float));
+
+    cl::sycl::queue q;
+    q.submit([&b, offset](cl::sycl::handler &h) {
+      auto accB = b.get_access<sycl_acc_rw>(h);
+      h.single_task<class foo1>([=]() {
+        cl::sycl::codeplay::get_device_ptr_as<float>(accB)[offset] = 1.0f;
+      });
+    });
+
+    // Only way of reading the value is using a host accessor
+    {
+      auto hostAcc = b.get_access<sycl_acc_rw, sycl_acc_host>();
+      ASSERT_EQ(cl::sycl::codeplay::get_host_ptr_as<float>(hostAcc)[offset],
+                1.0f);
+    }
+
+    SYCLfree(myPtr, pMap);
+    ASSERT_EQ(pMap.count(), 0u);
+  }
+}
+
+TEST(offset, 2d_indexing) {
+  PointerMapper pMap;
+  {
+    const unsigned SIZE = 8;
+    ASSERT_EQ(pMap.count(), 0u);
+    float *myPtr =
+        static_cast<float *>(SYCLmalloc(SIZE * SIZE * sizeof(float), pMap));
+
+    ASSERT_NE(myPtr, nullptr);
+
+    ASSERT_FALSE(PointerMapper::is_nullptr(myPtr));
+
+    ASSERT_EQ(pMap.count(), 1u);
+
+    cl::sycl::queue q;
+
+    float *actPos = myPtr;
+    for (unsigned i = 0; i < SIZE; i++) {
+      for (unsigned j = 0; j < SIZE; j++) {
+        // Obtain the buffer
+        // Note that the scope of this buffer ends when the buffer
+        // is freed
+        //
+        auto b = pMap.get_buffer(actPos);
+
+        size_t offset = pMap.get_offset(actPos);
+        ASSERT_EQ(offset, (i * SIZE + j) * sizeof(float));
+
+        q.submit([b, offset, i, j, SIZE](cl::sycl::handler &h) mutable {
+          auto accB = b.get_access<sycl_acc_rw>(h);
+          h.single_task(kernel(accB, i, j, SIZE, offset));
+        });
+
+        // We move to the next ptr
+        actPos++;
+      }  // for int j
+    }    // for int i
+
+    // Only way of reading the value is using a host accessor
+    {
+      auto b = pMap.get_buffer(myPtr);
+      ASSERT_EQ(b.get_size(), SIZE * SIZE * sizeof(float));
+      auto hostAcc = b.get_access<sycl_acc_rw, sycl_acc_host>();
+      float *fPtr = cl::sycl::codeplay::get_host_ptr_as<float>(hostAcc);
+      for (unsigned i = 0; i < SIZE; i++) {
+        for (unsigned j = 0; j < SIZE; j++) {
+          ASSERT_EQ(fPtr[i * SIZE + j], i * SIZE + j);
+        }
+      }
+    }
+    SYCLfree(myPtr, pMap);
+    ASSERT_EQ(pMap.count(), 0u);
+  }
+}
