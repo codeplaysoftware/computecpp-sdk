af861484916abffd6d5126019fad76304cb5ff8e
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2017-07-21 17:46:37 +0100

    Add Python script to extract IR from integration header

---
+++ tools/extract_ir/extract_ir.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+#
+#  Copyright (C) 2017 Codeplay Software Limited
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  For your convenience, a copy of the License has been included in this
+#  repository.
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  Codeplay's ComputeCpp SDK
+#
+#  extract_ir.py
+#
+#  Description:
+#    Script to extract SPIR or other IR from a SYCL integration header.
+#
+
+import os
+import argparse
+import sys
+import re
+import subprocess
+import tempfile
+import shutil
+import struct
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Finds an IR module in a SYCL integration header and writes it out to a binary file. If path to llvm-dis is provided, tries to disassemble the binary and write the disassembled text into the output file instead.')
+
+    parser.add_argument('-i', '--input-file',
+                        required=False,
+                        default=sys.stdin,
+                        type=argparse.FileType('r'),
+                        help='The header to process, defaults to stdin.')
+    parser.add_argument('-o', '--output-file',
+                        required=False,
+                        default='',
+                        help='The file to output to, defaults to stdout.')
+    parser.add_argument('--llvm-dis',
+                        required=False,
+                        default='',
+                        help='The path to llvm-dis. Bytecode will be disassembled only if this is provided.')
+    return parser.parse_args()
+
+# Try to use llvm-dis to disassemble the input file into the output file
+# Output defaults to stdout
+def try_disassemble_spir(llvm_dis_path, input_file, output_file='-'):
+        code = subprocess.call([llvm_dis_path,input_file,'-o='+output_file])
+
+        if code != 0:
+            raise RuntimeError('Error running llvm-dis! Error: ' + str(code))
+
+# Extracts the kernel IR binary from an integration header into the output file
+# Output file has to be open in binary mode
+# Returns the type of the IR as string or '' if unknown
+def extract_ir(input_file, output_file):
+    start_re = re.compile('unsigned char [A-Za-z-0-9_]*_bin_spir(64|32)\[\] = {')
+    spir32_re = re.compile('spir32')
+    spir64_re = re.compile('spir64')
+    end_line = '};\n'
+
+    reached_start = False
+    bin_type = ''
+
+    # Read the file line by line until we reach the start of the binary blob
+    for line in input_file:
+        if reached_start:
+            if line == end_line:
+                break
+            else:
+                line = line.replace(',','')
+                hex_list_as_string = line.split()
+
+                for byte in hex_list_as_string:
+                    output_file.write(struct.pack('B', int(byte[2:],16)))
+
+        if not reached_start and start_re.match(line):
+            reached_start = True
+            if spir32_re.search(line):
+                bin_type = 'spir32'
+            elif spir64_re.search(line):
+                bin_type = 'spir64'
+
+    for line in input_file:
+        # Make sure the header has been fully read
+        pass
+
+    return bin_type
+
+def main():
+    args = parse_args()
+    input_file = args.input_file
+    output_file_name = args.output_file
+    llvm_dis_path = args.llvm_dis
+
+    # We have to write to the file as binary, since otherwise it can
+    # add carriage returns on Windows, leading to an invalid binary.
+    bin_output_file = tempfile.NamedTemporaryFile(mode='wb',delete=False)
+    bin_type = extract_ir(input_file, bin_output_file)
+
+    bin_output_file.flush()
+    os.fsync(bin_output_file)
+    bin_output_file.close()
+
+    input_file.close()
+
+    if llvm_dis_path != '':
+        if bin_type == 'spir32' or bin_type == 'spir64':
+            if output_file_name != '':
+                # Output file name provided
+                try_disassemble_spir(llvm_dis_path, bin_output_file.name, output_file_name)
+            else:
+                # Otherwise default to stdout
+                try_disassemble_spir(llvm_dis_path, bin_output_file.name)
+        else:
+            sys.stderr.write('Unknown IR binary type found! Cannot disassemble.')
+            exit(1)
+    else:
+        # llvm-dis argument not provided, store binary IR
+        if output_file_name != '':
+            # into output file if provided
+            shutil.copyfile(bin_output_file.name, output_file_name)
+        else:
+            # otherwise write to stdout
+            bin_output_file = open(bin_output_file.name, 'rb')
+            # python2-3 compatbility requires sys.stdout on 2 and sys.stdout.buffer on 3
+            out = getattr(sys.stdout, 'buffer', sys.stdout)
+            out.write(bin_output_file.read())
+            bin_output_file.close()
+
+main()
