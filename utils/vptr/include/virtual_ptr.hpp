79a62cfb33a012a77d2b0f4bdedc3317512ed492
Ruyman <ruyman@codeplay.com>
2017-07-28 11:42:35 +0100

    Virtual Address Space implementation (#33)
    This commit implements the virtual pointer interface for buffers, that
    enables allocation and  deallocation of pointer-like structures that
    are not dereferenciable on the host but can be used to retrieve buffers
    and offsets later on.
    
    The PointerMapper is a normal object, unlike the singleton implemented
    for the legacy pointer interface. See the README for details.

---
+++ utils/vptr/include/virtual_ptr.hpp
@@ -0,0 +1,462 @@
+/***************************************************************************
+ *  Copyright (C) 2017 Codeplay Software Limited
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  For your convenience, a copy of the License has been included in this
+ *  repository.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Codeplay's ComputeCpp SDK
+ *
+ *  virtual_ptr.hpp
+ *
+ *  Description:
+ *    Interface for SYCL buffers to behave as a non-dereferenceable pointer
+ *
+ * Authors:
+ *
+ *    Ruyman Reyes   Codeplay Software Ltd.
+ *    Mehdi Goli     Codeplay Software Ltd.
+ *    Vanya Yaneva   Codeplay Software Ltd.
+ *
+ **************************************************************************/
+
+#include <CL/sycl.hpp>
+
+#include <queue>
+#include <unordered_map>
+
+namespace cl {
+namespace sycl {
+namespace codeplay {
+
+using buffer_data_type = uint8_t;
+using sycl_acc_target = cl::sycl::access::target;
+using sycl_acc_mode = cl::sycl::access::mode;
+
+/**
+ * PointerMapper
+ *  Associates fake pointers with buffers.
+ *
+ */
+class PointerMapper {
+ public:
+  using base_ptr_t = std::uintptr_t;
+
+  /* Structure of a virtual pointer
+   *
+   * |================================================|
+   * |               POINTER ADDRESS                  |
+   * |================================================|
+   */
+  struct virtual_pointer_t {
+    /* Type for the pointers
+    */
+    base_ptr_t m_contents;
+
+    /** Conversions from virtual_pointer_t to
+     * void * should just reinterpret_cast the integer number
+     */
+    operator void *() const { return reinterpret_cast<void *>(m_contents); }
+
+    /**
+     * Convert back to the integer number.
+     */
+    operator base_ptr_t() const { return m_contents; }
+
+    /**
+     * Add a certain value to the pointer to create a
+     * new pointer to that offset
+     */
+    virtual_pointer_t operator+(size_t off) { return m_contents + off; }
+
+    /**
+     * Numerical order for sorting pointers in containers.
+     */
+    bool operator<(virtual_pointer_t rhs) const {
+      return (reinterpret_cast<base_ptr_t>(m_contents) <
+              reinterpret_cast<base_ptr_t>(rhs.m_contents));
+    }
+
+    /**
+     * Numerical order for sorting pointers in containers
+     */
+    bool operator==(virtual_pointer_t rhs) const {
+      return (reinterpret_cast<base_ptr_t>(m_contents) ==
+              reinterpret_cast<base_ptr_t>(rhs.m_contents));
+    }
+
+    /**
+     * Simple forward to the equality overload.
+     */
+    bool operator!=(virtual_pointer_t rhs) const {
+      return !(this->operator==(rhs));
+    }
+
+    /**
+     * Converts a void * into a virtual pointer structure.
+     * Note that this will only work if the void * was
+     * already a virtual_pointer_t, but we have no way of
+     * checking
+     */
+    virtual_pointer_t(const void *ptr)
+        : m_contents(reinterpret_cast<base_ptr_t>(ptr)){};
+
+    /**
+     * Creates a virtual_pointer_t from the given integer
+     * number
+     */
+    virtual_pointer_t(base_ptr_t u) : m_contents(u){};
+  };
+
+  /* Definition of a null pointer
+   */
+  const virtual_pointer_t null_virtual_ptr = nullptr;
+
+  /**
+   * Whether if a pointer is null or not.
+   * A pointer is nullptr if the value is of null_virtual_ptr
+   */
+  static inline bool is_nullptr(virtual_pointer_t ptr) {
+    return (static_cast<void *>(ptr) == nullptr);
+  }
+
+  /* basic type for all buffers
+   */
+  using buffer_t = cl::sycl::buffer_mem;
+
+  /**
+   * Node that stores information about a device allocation.
+   * Nodes are sorted by size to organise a free list of nodes
+   * that can be recovered.
+   */
+  struct pMapNode_t {
+    buffer_t m_buffer;
+    size_t m_size;
+    bool m_free;
+
+    pMapNode_t(buffer_t b, size_t size, bool f)
+        : m_buffer{b}, m_size{size}, m_free{f} {
+      m_buffer.set_final_data(nullptr);
+    }
+
+    bool operator<=(const pMapNode_t &rhs) { return (m_size <= rhs.m_size); }
+  };
+
+  /** Storage of the pointer / buffer tree
+   */
+  using pointerMap_t = std::map<virtual_pointer_t, pMapNode_t>;
+
+  /**
+   * Obtain the insertion point in the pointer map for
+   * a pointer of the given size.
+   * \param requiredSize Size attemted to reclaim
+   */
+  typename pointerMap_t::iterator get_insertion_point(size_t requiredSize) {
+    typename pointerMap_t::iterator retVal;
+    bool reuse = false;
+    if (!m_freeList.empty()) {
+      // try to re-use an existing block
+      for (auto freeElem : m_freeList) {
+        if (freeElem->second.m_size >= requiredSize) {
+          retVal = freeElem;
+          reuse = true;
+          // Element is not going to be free anymore
+          m_freeList.erase(freeElem);
+          break;
+        }
+      }
+    }
+    if (!reuse) {
+      retVal = std::prev(m_pointerMap.end());
+    }
+    return retVal;
+  }
+
+  /**
+   * Returns an iterator to the node that stores the information
+   * of the given virtual pointer from the given pointer map structure.
+   * If pointer is not found, throws std::out_of_range.
+   * If the pointer map structure is empty, throws std::out_of_range
+   *
+   * \param pMap the pointerMap_t structure storing all the pointers
+   * \param virtual_pointer_ptr The virtual pointer to obtain the node of
+   * \throws std::out:of_range if the pointer is not found or pMap is empty
+   */
+  typename pointerMap_t::iterator get_node(const virtual_pointer_t ptr) {
+    if (this->count() == 0) {
+      throw std::out_of_range("There are no pointers allocated");
+    }
+    // The previous element to the lower bound is the node that
+    // holds this memory address
+    auto node = m_pointerMap.lower_bound(ptr);
+    // If the value of the pointer is not the one of the node
+    // then we return the previous one
+    if (node->first != ptr) {
+      if (node == std::begin(m_pointerMap)) {
+        throw std::out_of_range("The pointer is not registered in the map");
+      }
+      --node;
+    }
+    return node;
+  }
+
+  /* get_buffer.
+   * Returns a buffer from the map using the pointer address
+   */
+  cl::sycl::buffer<buffer_data_type, 1, cl::sycl::detail::base_allocator>
+  get_buffer(const virtual_pointer_t ptr) {
+    using buffer_t =
+        cl::sycl::buffer<buffer_data_type, 1, cl::sycl::detail::base_allocator>;
+
+    // get_node() returns a `buffer_mem`, so we need to cast it to a `buffer<>`.
+    // We can do this without the `buffer_mem` being a pointer, as we
+    // only declare member variables in the base class (`buffer_mem`) and not in
+    // the child class (`buffer<>).
+    buffer_t buf(*(static_cast<buffer_t *>(&get_node(ptr)->second.m_buffer)));
+    return buf;
+  }
+
+  /**
+   * @brief Returns an accessor to the buffer of the given virtual pointer
+   * @param accessMode
+   * @param accessTarget
+   * @param ptr The virtual pointer
+   */
+  template <sycl_acc_mode access_mode,
+            sycl_acc_target access_target = sycl_acc_target::global_buffer>
+  cl::sycl::accessor<buffer_data_type, 1, access_mode, access_target>
+  get_access(const virtual_pointer_t ptr) {
+    return get_buffer(ptr).get_access<access_mode, access_target>();
+  }
+
+  /**
+   * @brief Returns an accessor to the buffer of the given virtual pointer
+   *        in the given command group scope
+   * @param accessMode
+   * @param accessTarget
+   * @param ptr The virtual pointer
+   * @param cgh Reference to the command group scope
+   */
+  template <sycl_acc_mode access_mode,
+            sycl_acc_target access_target = sycl_acc_target::global_buffer>
+  cl::sycl::accessor<buffer_data_type, 1, access_mode, access_target>
+  get_access(const virtual_pointer_t ptr, cl::sycl::handler &cgh) {
+    return get_buffer(ptr).get_access<access_mode, access_target>(cgh);
+  }
+
+  /*
+   * Returns the offset from the base address of this pointer.
+   */
+  inline off_t get_offset(const virtual_pointer_t ptr) {
+    // The previous element to the lower bound is the node that
+    // holds this memory address
+    return (ptr - get_node(ptr)->first);
+  }
+
+  /**
+   * Constructs the PointerMapper structure.
+   */
+  PointerMapper() : m_pointerMap{}, m_freeList{} {};
+
+  /**
+   * PointerMapper cannot be copied or moved
+   */
+  PointerMapper(const PointerMapper &) = delete;
+
+  /**
+  *	empty the pointer list
+  */
+  inline void clear() {
+    m_freeList.clear();
+    m_pointerMap.clear();
+  }
+
+  /* add_pointer.
+   * Adds a pointer to the map and returns the virtual pointer id.
+   */
+  virtual_pointer_t add_pointer(buffer_t &&b) {
+    virtual_pointer_t retVal = nullptr;
+    size_t bufSize = b.get_count();
+    pMapNode_t p{b, bufSize, false};
+    // If this is the first pointer:
+    if (m_pointerMap.empty()) {
+      virtual_pointer_t initialVal{1};
+      m_pointerMap.emplace(initialVal, p);
+      return initialVal;
+    }
+    auto lastElemIter = get_insertion_point(bufSize);
+    // We are recovering an existing free node
+    if (lastElemIter->second.m_free) {
+      lastElemIter->second.m_buffer = b;
+      lastElemIter->second.m_free = false;
+
+      // If the recovered node is bigger than the inserted one
+      // add a new free node with the remaining space
+      if (lastElemIter->second.m_size > bufSize) {
+        // create a new node with the remaining space
+        auto remainingSize = lastElemIter->second.m_size - bufSize;
+        pMapNode_t p2{b, remainingSize, true};
+
+        // update size of the current node
+        lastElemIter->second.m_size = bufSize;
+
+        // add the new free node
+        auto newFreePtr = lastElemIter->first + bufSize;
+        auto freeNode = m_pointerMap.emplace(newFreePtr, p2).first;
+        m_freeList.emplace(freeNode);
+      }
+
+      retVal = lastElemIter->first;
+    } else {
+      size_t lastSize = lastElemIter->second.m_size;
+      retVal = lastElemIter->first + lastSize;
+      m_pointerMap.emplace(retVal, p);
+    }
+    return retVal;
+  }
+
+  /**
+   * @brief Fuses the given node with the previous nodes in the
+   *        pointer map if they are free
+   *
+   * @param node A reference to the free node to be fused
+   */
+  void fuse_forward(typename pointerMap_t::iterator &node) {
+    while (node != std::prev(m_pointerMap.end())) {
+      // if following node is free
+      // remove it and extend the current node with its size
+      auto fwd_node = std::next(node);
+      if (!fwd_node->second.m_free) {
+        break;
+      }
+      auto fwd_size = fwd_node->second.m_size;
+      m_freeList.erase(fwd_node);
+      m_pointerMap.erase(fwd_node);
+
+      node->second.m_size += fwd_size;
+    }
+  }
+
+  /**
+   * @brief Fuses the given node with the following nodes in the
+   *        pointer map if they are free
+   *
+   * @param node A reference to the free node to be fused
+   */
+  void fuse_backward(typename pointerMap_t::iterator &node) {
+    while (node != m_pointerMap.begin()) {
+      // if previous node is free, extend it
+      // with the size of the current one
+      auto prev_node = std::prev(node);
+      if (!prev_node->second.m_free) {
+        break;
+      }
+      prev_node->second.m_size += node->second.m_size;
+
+      // remove the current node
+      m_freeList.erase(node);
+      m_pointerMap.erase(node);
+
+      // point to the previous node
+      node = prev_node;
+    }
+  }
+
+  /* remove_pointer.
+   * Removes the given pointer from the map.
+   */
+  void remove_pointer(const virtual_pointer_t ptr) {
+    auto node = this->get_node(ptr);
+
+    node->second.m_free = true;
+    m_freeList.emplace(node);
+
+    // Fuse the node
+    // with free nodes before and after it
+    fuse_forward(node);
+    fuse_backward(node);
+
+    // If after fusing the node is the last one
+    // simply remove it (since it is free)
+    if (node == std::prev(m_pointerMap.end())) {
+      m_freeList.erase(node);
+      m_pointerMap.erase(node);
+    }
+  }
+
+  /* count.
+   * Return the number of active pointers (i.e, pointers that
+   * have been malloc but not freed).
+   */
+  size_t count() const { return (m_pointerMap.size() - m_freeList.size()); }
+
+ private:
+  /**
+   * Compare two iterators to pointer map entries according to
+   * the size of the allocation on the device.
+   */
+  struct SortBySize {
+    bool operator()(typename pointerMap_t::iterator a,
+                    typename pointerMap_t::iterator b) {
+      return ((a->first < b->first) && (a->second <= b->second)) ||
+             ((a->first < b->first) && (b->second <= a->second));
+    }
+  };
+
+  /* Maps the pointer addresses to buffer and size pairs.
+    */
+  pointerMap_t m_pointerMap;
+
+  /* List of free nodes available for re-using
+   */
+  std::set<typename pointerMap_t::iterator, SortBySize> m_freeList;
+};
+
+/**
+ * Malloc-like interface to the pointer-mapper.
+ * Given a size, creates a byte-typed buffer and returns a
+ * fake pointer to keep track of it.
+ * \param size Size in bytes of the desired allocation
+ * \throw cl::sycl::exception if error while creating the buffer
+ */
+template <
+    typename buffer_allocator = cl::sycl::default_allocator<buffer_data_type> >
+inline void *SYCLmalloc(size_t size, PointerMapper &pMap) {
+  // Create a generic buffer of the given size
+  using buffer_t = cl::sycl::buffer<buffer_data_type, 1, buffer_allocator>;
+  auto thePointer = pMap.add_pointer(buffer_t(cl::sycl::range<1>{size}));
+  // Store the buffer on the global list
+  return static_cast<void *>(thePointer);
+}
+
+/**
+ * Free-like interface to the pointer mapper.
+ * Given a fake-pointer created with the virtual-pointer malloc,
+ * destroys the buffer and remove it from the list.
+ */
+template <typename PointerMapper>
+inline void SYCLfree(void *ptr, PointerMapper &pMap) {
+  pMap.remove_pointer(ptr);
+}
+
+/**
+ * Clear all the memory allocated by SYCL.
+ */
+template <typename PointerMapper>
+inline void SYCLfreeAll(PointerMapper &pMap) {
+  pMap.clear();
+}
+
+}  // codeplay
+}  // sycl
+}  // cl
---
+++ utils/vptr/tests/space.cc
@@ -0,0 +1,215 @@
+/***************************************************************************
+ *
+ *  Copyright (C) 2016 Codeplay Software Limited
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  For your convenience, a copy of the License has been included in this
+ *  repository.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Codeplay's ComputeCpp SDK
+ *
+ *  space.cc
+ *
+ *  Description:
+ *   Tests for the space management of mapper
+ *
+ **************************************************************************/
+
+#include "gtest/gtest.h"
+
+#include <CL/sycl.hpp>
+#include <iostream>
+
+#include "pointer_alias.hpp"
+#include "virtual_ptr.hpp"
+
+using sycl_acc_target = cl::sycl::access::target;
+const sycl_acc_target sycl_acc_host = sycl_acc_target::host_buffer;
+const sycl_acc_target sycl_acc_buffer = sycl_acc_target::global_buffer;
+
+using sycl_acc_mode = cl::sycl::access::mode;
+const sycl_acc_mode sycl_acc_rw = sycl_acc_mode::read_write;
+
+using namespace cl::sycl::codeplay;
+
+using buffer_t = PointerMapper::buffer_t;
+
+int n = 10000;
+int startCount = 5;
+
+TEST(space, add_only) {
+  // Expect: memory usage grows
+  PointerMapper pMap;
+  {
+    float *ptrs[n];
+    for (int i = 0; i < n; i++) {
+      ptrs[i] = static_cast<float *>(SYCLmalloc(100 * sizeof(float), pMap));
+      ASSERT_EQ(pMap.count(), i + 1);
+    }
+  }
+}
+
+TEST(space, remove_in_order) {
+  // Expect: memory usage grows, then stays the same
+  PointerMapper pMap;
+  {
+    float *ptrs[n];
+
+    for (int i = 0; i < n; i++) {
+      ptrs[i] = static_cast<float *>(SYCLmalloc(100 * sizeof(float), pMap));
+      ASSERT_EQ(pMap.count(), i + 1);
+    }
+
+    for (int i = 0; i < n; i++) {
+      SYCLfree(ptrs[i], pMap);
+      ASSERT_EQ(pMap.count(), n - 1 - i);
+    }
+  }
+}
+
+TEST(space, remove_reverse_order) {
+  // Expect: memory usage grows, then goes down
+  PointerMapper pMap;
+  {
+    float *ptrs[n];
+
+    for (int i = 0; i < n; i++) {
+      ptrs[i] = static_cast<float *>(SYCLmalloc(100 * sizeof(float), pMap));
+      ASSERT_EQ(pMap.count(), i + 1);
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+      SYCLfree(ptrs[i], pMap);
+      ASSERT_EQ(pMap.count(), i);
+    }
+
+    ASSERT_EQ(pMap.count(), 0);
+  }
+}
+
+TEST(space, add_remove_same_size) {
+  // Expect: memory usage stays low
+  PointerMapper pMap;
+  {
+    float *ptrs[n];
+
+    for (int i = 0; i < startCount; i++) {
+      ptrs[i] = static_cast<float *>(SYCLmalloc(n * sizeof(float), pMap));
+      ASSERT_EQ(pMap.count(), i + 1);
+    }
+
+    for (int i = startCount; i < n; i++) {
+      SYCLfree(ptrs[i - startCount], pMap);
+      ptrs[i] = static_cast<float *>(SYCLmalloc(n * sizeof(float), pMap));
+      ASSERT_EQ(pMap.count(), startCount);
+    }
+  }
+}
+
+TEST(space, add_remove_decreasing_size) {
+  // Expect: memory usage grows
+  PointerMapper pMap;
+  {
+    float *ptrs[n];
+
+    for (int i = 0; i < startCount; i++) {
+      ptrs[i] = static_cast<float *>(SYCLmalloc(n * sizeof(float), pMap));
+      ASSERT_EQ(pMap.count(), i + 1);
+    }
+
+    for (int i = startCount; i < n; i++) {
+      SYCLfree(ptrs[i - startCount], pMap);
+      ptrs[i] =
+          static_cast<float *>(SYCLmalloc(1 * (n - i) * sizeof(float), pMap));
+      ASSERT_EQ(pMap.count(), startCount);
+    }
+  }
+}
+
+TEST(space, add_remove_increasing_size) {
+  // Expect: memory usage grows
+  PointerMapper pMap;
+  {
+    float *ptrs[n];
+
+    for (int i = 0; i < startCount; i++) {
+      ptrs[i] = static_cast<float *>(SYCLmalloc(1 * sizeof(float), pMap));
+      ASSERT_EQ(pMap.count(), i + 1);
+    }
+
+    for (int i = startCount; i < n; i++) {
+      SYCLfree(ptrs[i - startCount], pMap);
+      ptrs[i] = static_cast<float *>(SYCLmalloc(i * sizeof(float), pMap));
+      ASSERT_EQ(pMap.count(), startCount);
+    }
+  }
+}
+
+TEST(space, fragmentation) {
+  PointerMapper pMap;
+  {
+    auto length1 = 100;
+    auto length2 = 50;
+    auto length3 = 50;
+    auto length4 = 100;
+
+    auto ptr1 = static_cast<float *>(SYCLmalloc(length1 * sizeof(float), pMap));
+    auto ptr2 = static_cast<float *>(SYCLmalloc(length2 * sizeof(float), pMap));
+    auto ptr3 = static_cast<float *>(SYCLmalloc(length3 * sizeof(float), pMap));
+    auto ptr4 = static_cast<float *>(SYCLmalloc(length4 * sizeof(float), pMap));
+
+    // Remove the second pointer
+    SYCLfree(ptr2, pMap);
+    // The pointer is freed
+    ASSERT_TRUE(pMap.get_node(ptr2)->second.m_free);
+
+    /** Test fragmentation of free space when new node is added
+     *  to a free space of larger size **/
+    // Add a new pointer, half the size of the removed pointer
+    auto length5 = length2 / 2;
+    auto size5 = length5 * sizeof(float);
+    auto ptr5 = static_cast<float *>(SYCLmalloc(size5, pMap));
+    // New pointer reuses the space of the removed pointer
+    ASSERT_EQ(ptr2, ptr5);
+    // The remaining space is freed and of correct size
+    auto ptrFree = ptr5 + length5;
+    auto freeSize = length2 * sizeof(float) - size5;
+    ASSERT_TRUE(pMap.get_node(ptrFree)->second.m_free);
+    ASSERT_EQ(freeSize, pMap.get_node(ptrFree)->second.m_size);
+
+    /** Test fuse back when removing nodes **/
+    // Free the node **after** the new free space
+    // They are two separate nodes
+    ASSERT_NE(ptr3, ptrFree);
+    SYCLfree(ptr3, pMap);
+    // The two freed spaces are now fused
+    // they return the same free node
+    // and are of the same size
+    ASSERT_EQ(pMap.get_node(ptr3), pMap.get_node(ptrFree));
+    freeSize += length3 * sizeof(float);
+    ASSERT_EQ(freeSize, pMap.get_node(ptrFree)->second.m_size);
+
+    /** Test fuse forward when removing nodes **/
+    // Free the node **before** the free space
+    // They are two separate nodes
+    ASSERT_NE(ptrFree, ptr5);
+    SYCLfree(ptr5, pMap);
+    // The two freed spaces are now fused
+    // they return the same free node
+    // and are of the same size
+    ASSERT_EQ(pMap.get_node(ptr5), pMap.get_node(ptrFree));
+    ptrFree = ptr5;
+    freeSize += size5;
+    ASSERT_EQ(freeSize, pMap.get_node(ptrFree)->second.m_size);
+  }
+}
