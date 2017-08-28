eea4415411e125351677af8af30e8221b2549cc3
Vanya Yaneva <wyaneva@gmail.com>
2017-08-28 06:33:48 +0100

    Re-arranged template parameters in `get_buffer()`
    so that existing tests do not need to change

--- utils/vptr/include/virtual_ptr.hpp
+++ utils/vptr/include/virtual_ptr.hpp
@@ -222,8 +222,8 @@ class PointerMapper {
   /* get_buffer.
    * Returns a buffer from the map using the pointer address
    */
-  template <typename buffer_data_type = buffer_data_type_t,
-            typename buffer_allocator = cl::sycl::detail::base_allocator>
+  template <typename buffer_allocator = cl::sycl::detail::base_allocator,
+            typename buffer_data_type = buffer_data_type_t>
   cl::sycl::buffer<buffer_data_type, 1, buffer_allocator> get_buffer(
       const virtual_pointer_t ptr) {
     using buffer_t = cl::sycl::buffer<buffer_data_type, 1, buffer_allocator>;
