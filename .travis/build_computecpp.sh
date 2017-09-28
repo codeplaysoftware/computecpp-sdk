6a0199104a6a1c339af1b277a7b671d1a5a1ed49
Duncan McBain <duncan.m.mcbain@googlemail.com>
2017-09-28 10:06:05 +0000

    Update travis config for 0.3.2 (#56)
    Hopefully, this config will not break when a new version of ComputeCpp is
    released, as it now specifies the version of ComputeCpp to download.
    
    The clang configuration has been removed, as it is failing. Similarly,
    the example_vptr test will not be built until it is fixed (in a separate
    changeset).

--- .travis/build_computecpp.sh
+++ .travis/build_computecpp.sh
@@ -5,7 +5,7 @@ set -ev
 ###########################
 # Get ComputeCpp
 ###########################
-wget --no-check-certificate https://computecpp.codeplay.com/downloads/computecpp-ce/latest/ubuntu-14.04-64bit.tar.gz
-tar -xzf ubuntu-14.04-64bit.tar.gz -C /tmp 
+wget --no-check-certificate https://computecpp.codeplay.com/downloads/computecpp-ce/0.3.2/ubuntu-14.04-64bit.tar.gz
+tar -xzf ubuntu-14.04-64bit.tar.gz -C /tmp
 # Workaround for C99 definition conflict
 bash .travis/computecpp_workaround.sh
