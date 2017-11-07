2b955f248c7294ddf295eb7e1ac019cab0fc0281
Duncan McBain <duncan@codeplay.com>
2017-11-07 20:28:15 +0000

    Remove unused bash script from Travis

--- .travis/build_computecpp.sh
+++ .travis/build_computecpp.sh
@@ -8,4 +8,4 @@ set -ev
 wget --no-check-certificate https://computecpp.codeplay.com/downloads/computecpp-ce/0.3.3/ubuntu-14.04-64bit.tar.gz
 tar -xzf ubuntu-14.04-64bit.tar.gz -C /tmp
 # Workaround for C99 definition conflict
-bash .travis/computecpp_workaround.sh
+#bash .travis/computecpp_workaround.sh
