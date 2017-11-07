e0ea22a5ff5368c81dbc88db2f8882dc75031b50
Duncan McBain <duncan@codeplay.com>
2017-11-07 20:49:07 +0000

    Fix broken Travis config
    This will need to be partially reverted on the next release!

--- .travis/build_computecpp.sh
+++ .travis/build_computecpp.sh
@@ -9,4 +9,4 @@ wget --no-check-certificate https://computecpp.codeplay.com/downloads/computecpp
 tar -xzf ubuntu-14.04-64bit.tar.gz -C /tmp
 ls /tmp
 # Workaround for C99 definition conflict
-#bash .travis/computecpp_workaround.sh
+bash .travis/computecpp_workaround.sh
