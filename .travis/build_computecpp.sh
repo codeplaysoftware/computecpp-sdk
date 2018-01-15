0b2c850288bf058135d5872b2120ba8f50bd0a77
Duncan McBain <duncan@codeplay.com>
2018-01-15 20:49:41 +0000

    Fix bad download name

--- .travis/build_computecpp.sh
+++ .travis/build_computecpp.sh
@@ -5,7 +5,7 @@ set -ev
 ###########################
 # Get ComputeCpp
 ###########################
-wget --no-check-certificate https://computecpp.codeplay.com/downloads/computecpp-ce/0.5.1/ubuntu-16.04-64bit.tar.gz
+wget --no-check-certificate https://computecpp.codeplay.com/downloads/computecpp-ce/0.5.1/ubuntu-14.04-64bit.tar.gz
 tar -xzf ubuntu-14.04-64bit.tar.gz -C /tmp
 mv /tmp/ComputeCpp-CE-0.5.1-Ubuntu-14.04-64bit /tmp/computecpp
 # Workaround for C99 definition conflict
