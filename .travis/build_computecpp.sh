cda4e7a901247000e81c641838a882eb1592c2d0
Duncan McBain <duncan@codeplay.com>
2018-01-15 20:23:43 +0000

    Update the travis config for 0.5.1
    The opportunity was taken to update the Travis configuration such
    that the download script now places ComputeCpp into a known location.
    From now on, only the download script should require updates when a
    new version of ComputeCpp is released.

--- .travis/build_computecpp.sh
+++ .travis/build_computecpp.sh
@@ -5,7 +5,8 @@ set -ev
 ###########################
 # Get ComputeCpp
 ###########################
-wget --no-check-certificate https://computecpp.codeplay.com/downloads/computecpp-ce/0.5.0/ubuntu-14.04-64bit.tar.gz
-tar -xzf ubuntu-14.04-64bit.tar.gz -C /tmp
+wget --no-check-certificate https://computecpp.codeplay.com/downloads/computecpp-ce/0.5.1/ubuntu-16.04-64bit.tar.gz
+tar -xzf ubuntu-16.04-64bit.tar.gz -C /tmp
+mv /tmp/ComputeCpp-CE-0.5.0-Ubuntu-16.04-64bit /tmp/computecpp
 # Workaround for C99 definition conflict
 bash .travis/computecpp_workaround.sh
