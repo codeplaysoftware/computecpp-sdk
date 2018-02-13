22372d0728f0d2909c3c6f44186e43a1b1f8239e
Duncan McBain <duncan@codeplay.com>
2018-02-13 18:24:23 +0000

    Revert "Use 16.04 version of ComputeCpp with new GCC ABI"
    This reverts commit d9c016966ce82edc20d8b97fb689764d164d7c23.

--- .travis/build_computecpp.sh
+++ .travis/build_computecpp.sh
@@ -5,8 +5,8 @@ set -ev
 ###########################
 # Get ComputeCpp
 ###########################
-wget --no-check-certificate https://computecpp.codeplay.com/downloads/computecpp-ce/0.6.0/ubuntu-16.04-64bit.tar.gz
-tar -xzf ubuntu-16.04-64bit.tar.gz -C /tmp
-mv /tmp/ComputeCpp-CE-0.6.0-Ubuntu-16.04-64bit /tmp/computecpp
+wget --no-check-certificate https://computecpp.codeplay.com/downloads/computecpp-ce/0.6.0/ubuntu-14.04-64bit.tar.gz
+tar -xzf ubuntu-14.04-64bit.tar.gz -C /tmp
+mv /tmp/ComputeCpp-CE-0.6.0-Ubuntu-14.04-64bit /tmp/computecpp
 # Workaround for C99 definition conflict
 bash .travis/computecpp_workaround.sh
