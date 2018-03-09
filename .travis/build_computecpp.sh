80e40a83f701aae4f699b84da38c985827143582
Duncan McBain <duncan@codeplay.com>
2018-03-09 18:49:22 +0000

    Update travis configuration to download v0.6.1

--- .travis/build_computecpp.sh
+++ .travis/build_computecpp.sh
@@ -5,8 +5,8 @@ set -ev
 ###########################
 # Get ComputeCpp
 ###########################
-wget --no-check-certificate https://computecpp.codeplay.com/downloads/computecpp-ce/0.6.0/ubuntu-14.04-64bit.tar.gz
+wget --no-check-certificate https://computecpp.codeplay.com/downloads/computecpp-ce/0.6.1/ubuntu-14.04-64bit.tar.gz
 tar -xzf ubuntu-14.04-64bit.tar.gz -C /tmp
-mv /tmp/ComputeCpp-CE-0.6.0-Ubuntu-14.04-64bit /tmp/computecpp
+mv /tmp/ComputeCpp-CE-0.6.1-Ubuntu-14.04-64bit /tmp/computecpp
 # Workaround for C99 definition conflict
 bash .travis/computecpp_workaround.sh
