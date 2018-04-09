d582002150d1fb4c8aa0d03d86dd62f9324ffd8c
Duncan McBain <duncan@codeplay.com>
2018-04-09 17:19:24 +0100

    Update with review feedback
    * Remove default_selector, it's the same as the default queue ctor
    * Use normal variable initialisers in for loops
    * Return total number of failing elements as main return code
    * Expand comment describing what the reinterpret really does

--- samples/reinterpret/reinterpret.cpp
+++ samples/reinterpret/reinterpret.cpp
@@ -28,21 +28,24 @@
 #include <CL/sycl.hpp>
 
 int main() {
-  cl::sycl::default_selector selector;
   cl::sycl::range<1> r(128);
   cl::sycl::buffer<float, 1> buf_float(r);
-  cl::sycl::queue q(selector);
+  cl::sycl::queue q;
 
   {
-    auto acc = buf_float.get_access<cl::sycl::access::mode::read_write>();
-    for (auto i{0u}; i < r.size(); i++) {
+    auto acc = buf_float.get_access<cl::sycl::access::mode::write>();
+    for (auto i = 0u; i < r.size(); i++) {
       acc[i] = i + 1;
     }
   }
 
   /* buf_int is a new SYCL buffer, with the same total size as buf_float,
    * but will provide uint32_t elements instead. However, the device memory
-   * is the *same*. */
+   * is the *same*.
+   * This is actually true of the host memory, too, though there isn't any
+   * host memory explicitly used here. In effect the reinterpret is a way
+   * to obtain a differently-shaped handle over the top of SYCL's internal
+   * data structures. */
   auto buf_int = buf_float.reinterpret<uint32_t>(r);
   q.submit([&](cl::sycl::handler& cgh) {
     auto acc = buf_int.get_access<cl::sycl::access::mode::read_write>(cgh);
@@ -64,11 +67,11 @@ int main() {
    * 03-09-18-buffer-reinterpret-viewing-data-from-a-different-perspective */
   { auto acc = buf_int.get_access<cl::sycl::access::mode::read>(); }
 
-  bool ret = 0;
+  auto ret = 0;
   {
     auto acc = buf_float.get_access<cl::sycl::access::mode::read>();
-    for (auto i{0u}; i < r.size(); i++) {
-      ret |= (acc[i] != (2 * i + 2));
+    for (auto i = 0u; i < r.size(); i++) {
+      ret += (acc[i] != (2 * i + 2));
     }
   }
 
