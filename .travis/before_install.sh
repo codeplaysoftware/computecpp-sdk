7ac1e93d540f3ebff5604077b8644d5632c58587
Duncan McBain <duncan@codeplay.com>
2018-06-25 18:04:42 +0100

    Update Travis configuration
    Unfortunately we install an old version of CMake while travis appears
    to have a nice new one preinstalled which we should use instead.

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -2,9 +2,7 @@
 
 set -ev
 
-# Add modern dependencies (cmake, boost)
-sudo add-apt-repository ppa:george-edison55/cmake-3.x -y
 sudo apt-get update -q
-sudo apt-get install cmake ocl-icd-libopencl1 ocl-icd-dev opencl-headers -y 
+sudo apt-get install ocl-icd-libopencl1 ocl-icd-dev opencl-headers -y
 # Use gcc 5 as default
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
