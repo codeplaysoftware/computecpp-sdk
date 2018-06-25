3709e75f5197ff4310de1ab58bb6dc40441e4af9
Duncan McBain <duncan@codeplay.com>
2018-06-25 17:30:55 +0100

    Update CMake version required
    The minimum version of CMake needed is now 3.4.3. This version is
    still quite old, and is required by some other projects (e.g. LLVM).
    This might be a problem if you are still on Ubuntu 14.04 - please
    let us know if you are.

--- cmake/Modules/FindComputeCpp.cmake
+++ cmake/Modules/FindComputeCpp.cmake
@@ -29,7 +29,7 @@
 #  Latest version of this file can be found at:
 #    https://github.com/codeplaysoftware/computecpp-sdk
 
-cmake_minimum_required(VERSION 3.2.2)
+cmake_minimum_required(VERSION 3.4.3)
 include(FindPackageHandleStandardArgs)
 
 # Check that a supported host compiler can be found
