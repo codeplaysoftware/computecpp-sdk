9f09449cea2c83dedc4cdc16eac3e9470c046591
Duncan McBain <duncan.m.mcbain@googlemail.com>
2018-07-05 17:55:17 +0100

    Update SDK to ComputeCpp v0.9.0
    Obviously, the Travis change. Slightly less obviously, the debug
    build of ComputeCpp for Windows (which links against the debug CRT)
    is missing from the package. If you are building on Windows, you
    might get link errors for debug builds, or possibly even runtime
    crashes. If you do run into any errors, please let us know. It will
    be fixed in a subsequent release.

--- .travis/build_computecpp.sh
+++ .travis/build_computecpp.sh
@@ -3,7 +3,7 @@
 set -ev
 
 # Get ComputeCpp
-wget --no-check-certificate https://computecpp.codeplay.com/downloads/computecpp-ce/0.8.0/ubuntu-14.04-64bit.tar.gz
+wget --no-check-certificate https://computecpp.codeplay.com/downloads/computecpp-ce/0.9.0/ubuntu-14.04-64bit.tar.gz
 mkdir -p /tmp/computecpp
 tar -xzf ubuntu-14.04-64bit.tar.gz -C /tmp/computecpp --strip-components=1
 # Workaround for C99 definition conflict
