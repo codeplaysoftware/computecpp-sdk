957e7cf4e397b6e504256532dd0b7bf6905d8654
Duncan McBain <duncan@codeplay.com>
2018-07-05 17:21:22 +0100

    Use Khronos ICD loader in CMake configure

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -6,3 +6,11 @@ sudo apt-get update -q
 sudo apt-get install ocl-icd-libopencl1 ocl-icd-dev opencl-headers -y
 # Use gcc 5 as default
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
+cd /tmp
+git clone https://github.com/KhronosGroup/OpenCL-ICD-Loader
+cd OpenCL-ICD-Loader/inc
+git clone https://github.com/KhronosGroup/OpenCL-Headers
+cd ../
+mkdir -p build && cd build
+cmake ../ -DCMAKE_BUILD_TYPE=Release
+make -j2
