12e687f5050dca9403fe8fd1f1d29d97eb36027c
Duncan McBain <duncan@codeplay.com>
2018-07-05 17:25:45 +0100

    Fix clone path in OpenCL loader

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -8,9 +8,8 @@ sudo apt-get install ocl-icd-libopencl1 ocl-icd-dev opencl-headers -y
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
 cd /tmp
 git clone https://github.com/KhronosGroup/OpenCL-ICD-Loader
-cd OpenCL-ICD-Loader/inc
-git clone https://github.com/KhronosGroup/OpenCL-Headers
-cd ../
+cd OpenCL-ICD-Loader
+git clone https://github.com/KhronosGroup/OpenCL-Headers inc
 mkdir -p build && cd build
 cmake ../ -DCMAKE_BUILD_TYPE=Release
 make -j2
