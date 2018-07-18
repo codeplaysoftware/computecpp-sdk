8c4b221fa5c5a3e437a529f4c99f9dc39cd6cc60
Rod Burns <rodburns@users.noreply.github.com>
2018-07-18 11:15:29 +0100

    Update copyright year

--- cmake/Modules/FindComputeCpp.cmake
+++ cmake/Modules/FindComputeCpp.cmake
@@ -2,7 +2,7 @@
 # FindComputeCpp
 #---------------
 #
-#   Copyright 2016 Codeplay Software Ltd.
+#   Copyright 2016-2018 Codeplay Software Ltd.
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use these files except in compliance with the License.
