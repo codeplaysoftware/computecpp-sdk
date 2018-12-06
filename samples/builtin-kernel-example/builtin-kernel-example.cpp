29a2009aa214b74642e0cd2b772051de7d20d3d3
Ruyman Reyes <ruyman@codeplay.com>
2018-12-06 16:12:09 +0000

    Fixed queue and copyright

--- samples/builtin-kernel-example/builtin-kernel-example.cpp
+++ samples/builtin-kernel-example/builtin-kernel-example.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- *  Copyright (C) 2016 Codeplay Software Limited
+ *  Copyright (C) 2018 Codeplay Software Limited
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
@@ -31,9 +31,11 @@
 #include <array>
 #include <iostream>
 
+using namespace cl::sycl;
+
 int main() {
 
-  queue testQueue(TEST_SELECTOR, TEST_ASYNC_HANDLER);
+  queue testQueue;
   context testContext = testQueue.get_context();
 
   device dev = testContext.get_devices()[0];
