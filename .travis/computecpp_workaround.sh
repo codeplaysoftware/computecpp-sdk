69784964cf965ee9c0c9ee4c958fc53dbd550067
Duncan McBain <duncan@codeplay.com>
2019-01-23 11:48:12 +0000

    Update SDK to ComputeCpp v1.0.4

--- .travis/computecpp_workaround.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cat .travis/additional_undef /tmp/computecpp/include/SYCL/sycl_builtins.h > /tmp/tmp_builtins
-mv /tmp/tmp_builtins /tmp/computecpp/include/SYCL/sycl_builtins.h
-
-cat .travis/additional_undef /tmp/computecpp/include/SYCL/host_relational_builtins.h > /tmp/tmp_builtins
-mv /tmp/tmp_builtins /tmp/computecpp/include/SYCL/host_relational_builtins.h
