d2dc19964b7032801f022cd5d8e205f428d162e2
Duncan McBain <duncan@codeplay.com>
2019-03-05 13:19:16 +0000

    Update SDK to ComputeCpp v1.1.0

--- .travis/build_computecpp.sh
+++ .travis/build_computecpp.sh
@@ -2,7 +2,6 @@
 
 set -ev
 
-# Get ComputeCpp
-wget --no-check-certificate https://computecpp.codeplay.com/downloads/computecpp-ce/1.0.5/ubuntu-14.04-64bit.tar.gz
+wget --no-check-certificate https://computecpp.codeplay.com/downloads/computecpp-ce/1.1.0/ubuntu-14.04-64bit.tar.gz
 mkdir -p /tmp/computecpp
 tar -xzf ubuntu-14.04-64bit.tar.gz -C /tmp/computecpp --strip-components=1
