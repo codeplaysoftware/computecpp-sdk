6e7ba034dd1c606972fea463f94299790ebea07d
Duncan McBain <duncan@codeplay.com>
2019-03-05 16:39:47 +0000

    Update to Travis' Ubuntu Xenial

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 set -ev
-
 sudo apt-get update -q
 sudo apt-get install ocl-icd-libopencl1 ocl-icd-dev opencl-headers -y
-# Use gcc 5 as default
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
