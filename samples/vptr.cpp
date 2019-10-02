4494342a900dd90402607e708eb0a4d4c49f85bb
Kostas <kostas.kyrimis@codeplay.com>
2019-10-02 12:19:40 +0100

    updated vptr

--- samples/vptr.cpp
+++ samples/vptr.cpp
@@ -29,18 +29,11 @@
 #include <CL/sycl.hpp>
 
 #include <iostream>
-
 #include <vptr/virtual_ptr.hpp>
-
-using namespace cl::sycl;
 
 class init_a;
 class init_b;
 class matrix_add;
-
-const size_t N = 100;
-const size_t M = 150;
-
 /* This sample allocates three device-only matrices, using the virtual pointer
  * and SYCLmalloc. It initalises the first two in parallel on the device. After
  * that, it adds them together, storing the result in the third matrix. It then
@@ -48,73 +41,91 @@ const size_t M = 150;
  *  - pointer arithmetic on the virtual pointer to index the matrix
  *  - host accessor to gain access to the data. */
 int main() {
+  using namespace cl::sycl;
+  
+  constexpr size_t row = 100;
+  constexpr size_t col = 150;
+  constexpr size_t totalEntries = row * col;
+  constexpr size_t totalBytes = row * col * sizeof(float);
+  constexpr float multiplier1 = 2.0f;
+  constexpr float multiplier2 = 2014.0f;
   {
     queue myQueue;
     cl::sycl::codeplay::PointerMapper pMap;
 
-    /* Allocate the matrices using SYCLmalloc. a, b and c are virtual pointers,
-     * pointing to device buffers.
-     */
-    float* a = static_cast<float*>(SYCLmalloc(N * M * sizeof(float), pMap));
-    float* b = static_cast<float*>(SYCLmalloc(N * M * sizeof(float), pMap));
-    float* c = static_cast<float*>(SYCLmalloc(N * M * sizeof(float), pMap));
+    // Allocate the matrices using SYCLmalloc. a, b and c are virtual pointers,
+    // pointing to device buffers.
+    float* vptrA = static_cast<float*>(SYCLmalloc(totalBytes, pMap));
+    float* vptrB = static_cast<float*>(SYCLmalloc(totalBytes, pMap));
+    float* vptrC = static_cast<float*>(SYCLmalloc(totalBytes, pMap));
 
-    /* This kernel will initialise the buffer pointed to by a. The accessor "A"
-     * has write access. We retrieve it directly from the PointerMapper, using
-     * the virtual pointer.*/
+    // This kernel will initialise the buffer pointed to by a. The accessor "A"
+    // has write access. We retrieve it directly from the PointerMapper, using
+    // the virtual pointer.
     myQueue.submit([&](handler& cgh) {
+      constexpr size_t dim = 1;
+
       auto accA = pMap.get_access<access::mode::discard_write,
-                                  access::target::global_buffer, float>(a, cgh);
-      cgh.parallel_for<init_a>(
-          range<1>{N * M}, [=](item<1> index) { accA[index] = index[0] * 2; });
+                                  access::target::global_buffer, float>(vptrA, cgh);
+      auto functor = [=](item<dim> index) {
+                       accA[index] = index[0] * multiplier1;
+                     };
+
+      cgh.parallel_for<init_a>(range<dim>{totalEntries}, functor);
     });
 
-    /* Similarly, this kernel will initialise the buffer pointed to by b. */
+    //Similarly, this kernel will initialise the buffer pointed to by b.
     myQueue.submit([&](handler& cgh) {
-      auto accB = pMap.get_access<access::mode::discard_write,
-                                  access::target::global_buffer, float>(b, cgh);
-      cgh.parallel_for<init_b>(range<1>{N * M}, [=](item<1> index) {
-        accB[index] = index[0] * 2014;
-      });
-    });
+      constexpr size_t dim = 1;      
 
-    /* This kernel will perform the computation c = a + b. */
+      auto accB = pMap.get_access<access::mode::discard_write,
+                                  access::target::global_buffer, float>(vptrB, cgh);
+      auto functor = [=](item<dim> index) {
+                       accB[index] = index[0] * multiplier2;
+                     };
+
+      cgh.parallel_for<init_b>(range<dim>{totalEntries}, functor);
+      });
+
+    //This kernel will perform the computation c = a + b.
     myQueue.submit([&](handler& cgh) {
       auto accA = pMap.get_access<access::mode::read,
-                                  access::target::global_buffer, float>(a, cgh);
+                                  access::target::global_buffer, float>(vptrA, cgh);
       auto accB = pMap.get_access<access::mode::read,
-                                  access::target::global_buffer, float>(b, cgh);
+                                  access::target::global_buffer, float>(vptrB, cgh);
       auto accC = pMap.get_access<access::mode::discard_write,
-                                  access::target::global_buffer, float>(c, cgh);
-      cgh.parallel_for<matrix_add>(range<1>{N * M}, [=](item<1> index) {
-        accC[index] = accA[index] + accB[index];
-      });
+                                  access::target::global_buffer, float>(vptrC, cgh);
+
+      constexpr size_t dim = 1;
+      auto functor = [=](item<dim> index) {
+                       accC[index] = accA[index] + accB[index];
+                     };
+      
+      cgh.parallel_for<matrix_add>(range<dim>{totalEntries}, functor);
     });
 
-    /* On the host, the result stored in the buffer of virtual pointer "c" are
-     * checked. The matrix is accessed row by row, using pointer arithmetics on
-     * the virtual pointer. */
-    auto c_row = c;
-    for (size_t i = 0; i < N; i++) {
+    // On the host, the result stored in the buffer of virtual pointer "c" are
+    // checked. The matrix is accessed row by row, using pointer arithmetics on
+    // the virtual pointer.
+    auto c_row = vptrC;
+    for (size_t i = 0; i < row; ++i) {
       /* Get the number of elements by which the row is offset. */
       auto row_offset = pMap.get_element_offset<float>(c_row);
 
       /* Create a host accessor to access the data on the host. */
       auto accC = pMap.get_access<access::mode::read,
                                   access::target::host_buffer, float>(c_row);
-      for (size_t j = 0; j < M; j++) {
-        if (accC[row_offset + j] != (i * M + j) * (2 + 2014)) {
+      for (size_t j = 0; j < col; ++j) {
+        if (accC[row_offset + j] != (i * col + j) * (multiplier1 + multiplier2)) {
           std::cout << "Wrong value " << accC[row_offset + j] << " for element "
-                    << i * M + j << std::endl;
-          return -1;
+                    << i * col + j << '\n';
+          break;
         }
-        c_row++;
+        ++c_row;
       }
     }
-    /* End scope of myQueue, this waits for any remaining operations on the
-     * queue to complete. */
+    // End scope of myQueue, this waits for any remaining operations on the
+    // queue to complete.
   }
-
-  std::cout << "Good computation!" << std::endl;
   return 0;
 }
