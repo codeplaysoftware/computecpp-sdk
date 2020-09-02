257e259f70dcff1f8c299ff6e802808b3c58fa2f
Thales Sabino <thales@codeplay.com>
2020-09-02 10:06:54 +0100

    Group options into a variable to save lines

--- cmake/Modules/ComputeCppCompilerChecks.cmake
+++ cmake/Modules/ComputeCppCompilerChecks.cmake
@@ -13,16 +13,17 @@ endif()
 if(MSVC)
   set(ComputeCpp_STL_CHECK_SRC __STL_check)
   file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/${ComputeCpp_STL_CHECK_SRC}.cpp
-    "#include <CL/sycl.hpp>\n"
+    "#include <CL/sycl.hpp>  \n"
     "int main() { return 0; }\n")
+  set(_stl_test_command ${ComputeCpp_DEVICE_COMPILER_EXECUTABLE}
+                        -sycl
+                        ${COMPUTECPP_DEVICE_COMPILER_FLAGS}
+                        -isystem ${ComputeCpp_INCLUDE_DIRS}
+                        -isystem ${OpenCL_INCLUDE_DIRS}
+                        -o ${ComputeCpp_STL_CHECK_SRC}.sycl
+                        -c ${ComputeCpp_STL_CHECK_SRC}.cpp)
   execute_process(
-    COMMAND ${ComputeCpp_DEVICE_COMPILER_EXECUTABLE}
-            -sycl
-            ${COMPUTECPP_DEVICE_COMPILER_FLAGS}
-            -isystem ${ComputeCpp_INCLUDE_DIRS}
-            -isystem ${OpenCL_INCLUDE_DIRS}
-            -o ${ComputeCpp_STL_CHECK_SRC}.sycl
-            -c ${ComputeCpp_STL_CHECK_SRC}.cpp
+    COMMAND ${_stl_test_command}
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
     RESULT_VARIABLE ComputeCpp_STL_CHECK_RESULT
     ERROR_QUIET
@@ -30,31 +31,19 @@ if(MSVC)
   if(NOT ${ComputeCpp_STL_CHECK_RESULT} EQUAL 0)
     # Try disabling compiler version checks
     execute_process(
-      COMMAND ${ComputeCpp_DEVICE_COMPILER_EXECUTABLE}
-              -sycl
-              ${COMPUTECPP_DEVICE_COMPILER_FLAGS}
+      COMMAND ${_stl_test_command}
               -D_ALLOW_COMPILER_AND_STL_VERSION_MISMATCH
-              -isystem ${ComputeCpp_INCLUDE_DIRS}
-              -isystem ${OpenCL_INCLUDE_DIRS}
-              -o ${ComputeCpp_STL_CHECK_SRC}.cpp.sycl
-              -c ${ComputeCpp_STL_CHECK_SRC}.cpp
       WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
       RESULT_VARIABLE ComputeCpp_STL_CHECK_RESULT
       ERROR_QUIET
       OUTPUT_QUIET)
     if(NOT ${ComputeCpp_STL_CHECK_RESULT} EQUAL 0)
-      # Try again with __CUDACC__ and HAS_CONDITIONAL_EXPLICIT=0. This relaxes the restritions in the MSVC headers
+      # Try again with __CUDACC__ and _HAS_CONDITIONAL_EXPLICIT=0. This relaxes the restritions in the MSVC headers
       execute_process(
-        COMMAND ${ComputeCpp_DEVICE_COMPILER_EXECUTABLE}
-                -sycl
-                ${COMPUTECPP_DEVICE_COMPILER_FLAGS}
+        COMMAND ${_stl_test_command}
                 -D_ALLOW_COMPILER_AND_STL_VERSION_MISMATCH
                 -D_HAS_CONDITIONAL_EXPLICIT=0
                 -D__CUDACC__
-                -isystem ${ComputeCpp_INCLUDE_DIRS}
-                -isystem ${OpenCL_INCLUDE_DIRS}
-                -o ${ComputeCpp_STL_CHECK_SRC}.cpp.sycl
-                -c ${ComputeCpp_STL_CHECK_SRC}.cpp
         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
         RESULT_VARIABLE ComputeCpp_STL_CHECK_RESULT
         ERROR_QUIET
